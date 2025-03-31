34a43438e3618fdbfc4de0183a904ba57a581e02
joaopedroldavid-del <joaopedrolouren97@gmail.com>
2025-03-31 19:59:35 -0300

    qr code generation

--- payments/pix.py
+++ payments/pix.py
@@ -7,11 +7,11 @@ class Pix:
 
     def create_payment(self):
         # Simulation of a financial institute
-        bank_payment_id = uuid.uuid4()
+        bank_payment_id = str(uuid.uuid4())
         hash_payment = f'hash_payment_{bank_payment_id}'
 
         img = qrcode.make(hash_payment)
         img.save(f"static/img/qr_code_payment_{bank_payment_id}.png")
 
         return {"bank_payment_id": bank_payment_id,
-                "qr_code_path:": f"qr_code_payment_{bank_payment_id}"}
+                "qr_code_path": f"qr_code_payment_{bank_payment_id}"}
